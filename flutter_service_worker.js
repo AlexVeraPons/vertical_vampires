8d988c2235feb6de304401cbf5947f1097ea246d
AlexVeraPons <107070295+AlexVeraPons@users.noreply.github.com>
2025-02-20 13:35:35 +0100

    retry

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "5cd1776e1db0c8aa5a2bb144affb2eb1
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "cbe7ed28533a5f96626d20cb56ac6914",
+"flutter_bootstrap.js": "11cfe465abea41c57c1ccaf30ab289b8",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
